cb672629c2e271a72e06997083fd5e2744c41078
cuycorp <cuycorp@gmail.com>
2026-01-22 13:45:14 +0100

    added critical test

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,5 +1,7 @@
 #include "Span.hpp"
 #include "vector"
+#include <cstdlib> // for rand()
+#include <ctime>   // for time()
 
 #define RESET   "\033[0m"
 #define PINK    "\033[35m"
@@ -19,6 +21,7 @@ int main()
     std::cout << sp.longestSpan() << std::endl;
     std::cout << std::endl;
 
+    
     std::cout << PINK <<"Testing addNumber, which adds element from container" << RESET << std::endl;
     int arr[] = {0, 23, 1, 5};
     std::vector<int> va(arr, arr + 4);
@@ -33,5 +36,20 @@ int main()
     std::cout << "passed \n" ;
     std::cout << sp1.shortestSpan() << std::endl;
     std::cout << sp1.longestSpan() << std::endl;
+
+
+    std::cout << PINK << "Testing Span with 10,000 random numbers" << RESET << std::endl;
+    std::srand(std::time(NULL));
+    const int LARGE_SIZE = 10000;
+    Span spLarge(LARGE_SIZE);
+
+    for (int i = 0; i < LARGE_SIZE; ++i)
+        spLarge.addNumber(std::rand()); // add random numbers
+
+    std::cout << "passed\n";
+    std::cout << "Shortest span: " << spLarge.shortestSpan() << std::endl;
+    std::cout << "Longest span: " << spLarge.longestSpan() << std::endl;
+    std::cout << std::endl;
+
     return 0;
 }
