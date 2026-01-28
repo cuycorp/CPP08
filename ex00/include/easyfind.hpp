3470576292538861a6cf3426e3b6f619c796264e
cuycorp <cuycorp@gmail.com>
2026-01-28 16:20:36 +0100

    Added comment descriptions/

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -1,7 +1,8 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
-
+/* for find() */
 #include <algorithm>
+/* for exception */
 #include <exception>
 
 
@@ -9,7 +10,10 @@ template < typename containerT>
 bool ft_easyfind(containerT container, int elem)
 {
     if (std::find(container.begin(), container.end(), elem) != container.end())
+    {
+        std::
         return (true);
+    }
     else
         throw std::runtime_error("No element found!");
 }
